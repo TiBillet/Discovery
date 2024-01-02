87ddd66243f6b01cbbecab128c742169e9e6a8be
glen <glen84_dr@hotmail.com>
2024-01-02 22:24:25 +0400

    sending url as json

--- cryptographie/urls.py
+++ cryptographie/urls.py
@@ -3,5 +3,5 @@ from . import views
 from django.conf import settings
 
 urlpatterns = [
-
+    path('<pin_link>', views.send_url_based_on_pin, name='pin_link'),
 ]
--- cryptographie/views.py
+++ cryptographie/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import PrimaryLink
+import json
+from django.http import JsonResponse
 
-# Create your views here.
+# from the url the function will get the pin and will elaborate it
+# to get the url from the data and will send a json with the url
+def send_url_based_on_pin(request, pin_link):
+    primary_link = PrimaryLink.objects.get(server_url=pin_link)
+    data = []
+    data.append({
+        'url': primary_link.server_url
+    })
+    return JsonResponse(data, safe=False)

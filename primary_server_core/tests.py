9e11fc8881cd0c8807aac791306c2a689edbea1c
glen <glen84_dr@hotmail.com>
2024-01-05 19:03:03 +0400

    test 1 OK

--- primary_server_core/tests.py
+++ primary_server_core/tests.py
@@ -1,15 +1,19 @@
 from django.test import TestCase
-from primary_server_core.models import PrimaryLink
+from primary_server_core.models import PrimaryLink, CustomUser
 
 
 # Creating the PrimaryLink test clasx
-class PrimaryLink(TestCase):
+class PrimaryLinkCase(TestCase):
+    # creating a primary_link object
     def setUp(self):
-        pass
-        #self.primary_link = PrimaryLink.objects.create(pin_code="789112", server_url="https://tibillet.org")
-
+        PrimaryLink.objects.create(pin_code="789112", server_url="https://tibillet.org/")
 
     # Testing primarylink
     def test_primary_link(self):
-       pass
-       #self.assertEqual(self.primary_link.pin_code, '789112')
+        pin1 = PrimaryLink.objects.get(pin_code='789112')
+        # Testing when the code and the server url is right
+        self.assertEqual(pin1.pin_code, '789112')
+        self.assertEqual(pin1.server_url, 'https://tibillet.org/')
+        # testing when the pin code and the server_url is wrong
+        self.assertNotEquals(pin1.pin_code, '123456')
+        self.assertNotEquals(pin1.server_url, 'https://yahoo.com/')

c6890fa5c03e7fe21cc59ecd770311d290671a42
glen <glen84_dr@hotmail.com>
2024-01-05 20:42:33 +0400

    test 1 OK

--- primary_server_core/tests.py
+++ primary_server_core/tests.py
@@ -17,3 +17,14 @@ class PrimaryLinkCase(TestCase):
         # testing when the pin code and the server_url is wrong
         self.assertNotEquals(pin1.pin_code, '123456')
         self.assertNotEquals(pin1.server_url, 'https://yahoo.com/')
+
+# Testing the POST méthod:
+class TestCaseForm(TestCase):
+    # launching setUp
+    def setUp(self):
+        PrimaryLink.objects.create(pin_code="789112", server_url="https://tibillet.org/")
+
+
+    # creating the method that will test the POST
+    def test_post(self):
+        pass

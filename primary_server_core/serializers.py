be8de03e14527ddffea943aef0f9b58f9094d12f
Jonas Legion <jturbeaux@pm.me>
2024-02-22 16:33:04 +0100

    while code pin

--- primary_server_core/serializers.py
+++ primary_server_core/serializers.py
@@ -1,8 +1,31 @@
+import logging
+
+import cryptography.hazmat.primitives
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
+from rest_framework import serializers
 from rest_framework.generics import get_object_or_404
+from cryptography.hazmat.primitives import serialization
 
 from .models import PrimaryLink
-from rest_framework import serializers
 
+logger = logging.getLogger(__name__)
+
+
+def get_public_key(public_key_pem: str) -> rsa.RSAPublicKey | bool:
+    try:
+        # Charger la clé publique au format PEM
+        public_key = serialization.load_pem_public_key(public_key_pem.encode('utf-8'), backend=default_backend())
+
+        # Vérifier que la clé publique est au format RSA
+        if not isinstance(public_key, rsa.RSAPublicKey):
+            return False
+        return public_key
+
+    except Exception as e:
+        logger.error(f"Erreur de validation get_public_key : {e}")
+        raise e
 
 # Validation of the pin code
 class PinValidator(serializers.Serializer):
@@ -12,5 +35,21 @@ class PinValidator(serializers.Serializer):
     def validate_pinCode(self, value):
         self.link = get_object_or_404(PrimaryLink, pin_code=value)
         return value
+
+
+class LinkValidator(serializers.Serializer):
+    server_url = serializers.URLField(max_length=200, required=True)
+    rsa_pub_pem = serializers.CharField(max_length=512, required=True)
+    locale = serializers.CharField(max_length=2, required=True)
+
+    def rsa_pub_pem_validator(self, value):
+        try :
+            pub = get_public_key(value)
+            if pub.key_size < 2048:
+                raise serializers.ValidationError("Public key size too small")
+        except Exception as e:
+            raise serializers.ValidationError("Public key not valid, must be 2048 min rsa key")
+        return value
+
 
 
--- primary_server_core/views.py
+++ primary_server_core/views.py
@@ -1,16 +1,21 @@
+import random
+
+from rest_framework.throttling import AnonRateThrottle
+
 from .models import PrimaryLink
-from .serializers import PinValidator
+from .serializers import PinValidator, LinkValidator
 import json, requests
 from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, throttle_classes
 
 
 # from the url the function will get the pin and will elaborate it
 # to get the url from the data and will send a json with the server_url
 # @csrf_exempt
 @api_view(['POST'])
+@throttle_classes([AnonRateThrottle])
 def send_url_based_on_pin(request):
     # passing the pin code to the Validator
     pin_code_validator = PinValidator(data=request.data)
@@ -28,3 +33,21 @@ def send_url_based_on_pin(request):
 
     # send the server_url
     return Response(data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@throttle_classes([AnonRateThrottle])
+def create_link(request):
+    new_link = LinkValidator(data=request.data)
+    if not new_link.is_valid():
+        return Response(new_link.errors, status=status.HTTP_400_BAD_REQUEST)
+    validated_data = new_link.validated_data
+
+    # Génération du code pin
+    validated_data['pin_code'] = random.randint(100000, 999999)
+    while PrimaryLink.objects.filter(pin_code=validated_data['pin_code']).exists():
+        validated_data['pin_code'] = random.randint(100000, 999999)
+
+    link = PrimaryLink.objects.create(**validated_data)
+
+    return Response({"pin_code": link.pin_code}, status=status.HTTP_201_CREATED)
